bfc1383567b9b9852b7d83282e5a6fb5092ead3d
Softwarica College Lab <softwarica@softwarica.com>
2019-05-20 12:10:48 +0545

    add image in database

--- app/src/main/java/com/example/heroapi/RecyclerHeroes.java
+++ app/src/main/java/com/example/heroapi/RecyclerHeroes.java
@@ -3,9 +3,11 @@ package com.example.heroapi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
 public class RecyclerHeroes extends AppCompatActivity {
     RecyclerView recyclerView;
+    TextView textView ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
